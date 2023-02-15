a1a3f59ad15af26c4bee77bcb1fe85cdeec45d5d
Tigas Migas <tiago.m.mesquita@gmail.com>
2023-02-15 21:05:36 +0000

    1 resultado apenas, corrigi a formula b^2,

--- reso.py
+++ reso.py
@@ -2,16 +2,21 @@ import math
 
 def formula_resolvente(a,b,c):
 
-    insqrt = b^2 - 4*a*c
+    insqrt = b*b - 4*a*c
 
-    if (insqrt) < 0 :
+    if insqrt < 0 :
         return None
 
     xpos= (- b + math.sqrt(insqrt)) / (2 * a)
 
+    if insqrt == 0:
+        return (xpos, )
+
     xneg= (- b - math.sqrt(insqrt)) / (2 * a)
 
     return (xpos,xneg)
+
+print("ax^2 + bx + c = 0")
 
 print("ᐶ a: ")
 x = int(input())
@@ -26,6 +31,7 @@ resultado = formula_resolvente(x,y,z)
 
 if resultado == None:
     print("Não Existem Raízes Possíveis")
+elif len(resultado) == 1:
+    print("x = " + str(resultado[0]))
 else:
-    print(formula_resolvente(x,y,z))
-
+    print("x = " + str(resultado[0]) + ", x = " + str(resultado[1]))
